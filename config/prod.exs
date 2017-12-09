0402d400135bd3662f6d214ba0f5700ac9e2f6d9
Travis Smith <travis.alexander.smith.ja@gmail.com>
2017-12-08 22:26:18 -0500

    Updates Procfile

--- config/prod.exs
+++ config/prod.exs
@@ -15,8 +15,8 @@ use Mix.Config
 # which you typically run after static files are built.
 config :bakesters, BakestersWeb.Endpoint,
   url: [scheme: "https", host: "bakers-finesse.herokuapp.com", port: 443],
-  force_ssl: [rewrite_on: [:x_forwarded_proto]]
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
+  cache_static_manifest: "priv/static/cache_manifest.json",
   secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 config :bakesters, Bakesters.Repo,

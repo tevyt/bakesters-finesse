c3eddb3a2a6ad62a034ea71cb15c6f51ad136f22
Travis Smith <travis.alexander.smith.ja@gmail.com>
2017-12-10 22:32:02 -0500

    Completes initial spec

--- assets/js/react-app/components/header/tests/view.spec.js
+++ assets/js/react-app/components/header/tests/view.spec.js
@@ -1,16 +1,17 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import { mount } from 'enzyme';
 import { expect } from 'chai';
 
 import Header, { Logo } from '..';
 
 
 describe('Header', () => {
-  const wrapper = shallow(<Header />);
+  const wrapper = mount(<Header />);
   it('renders', () => {
     expect(wrapper).to.be.ok;
   });
   it('contains a logo', () => {
+    expect(wrapper.find('img')).to.have.lengthOf(1);
   });
   it('contains social media links');
   it('contains the bakery location');

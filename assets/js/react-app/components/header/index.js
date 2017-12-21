3d5e8961a8acbed16c1ea8124ba976b4b29dbc40
Travis Smith <travis.alexander.smith.ja@gmail.com>
2017-12-20 21:51:29 -0500

    Location in header links to social media

--- assets/js/react-app/components/header/index.js
+++ assets/js/react-app/components/header/index.js
@@ -44,7 +44,8 @@ const Links = styled.div`
   `}
 `;
 
-const Location = styled.div`
+const Location = styled.a`
+  display: block;
   color: ${colors.green}
 `;
 
@@ -54,11 +55,12 @@ const SocialMediaLinks = styled.div`
 `;
 
 export default (props) => {
+  const bakeryLocation = 'https://www.google.com.jm/maps/place/Ward+Ave,+Mandeville/@18.083396,-76.8086615,15z/data=!4m5!3m4!1s0x8edbbe34601591cb:0x7d1b624e04d1b456!8m2!3d18.0389678!4d-77.5133997?hl=en';
   return (
     <Container>
       <Logo src={images.bakestersLogo} />
       <Links>
-        <Location id='bakery-location'>Ward Avenue, Mandeville Jamaica</Location>
+        <Location id='bakery-location' href={bakeryLocation} target="_blank">Ward Avenue, Mandeville Jamaica</Location>
         <SocialMediaLinks>
           <SocialMediaLink provider={providers.instagram} profile='https://www.instagram.com/bakestersfinesse/' />
           <SocialMediaLink provider={providers.facebook} profile='https://www.facebook.com/Bakestersfinesse/' />

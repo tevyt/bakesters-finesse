a2b81aad56d7a261e993e999fcb87f56976ce016
Travis Smith <travis.alexander.smith.ja@gmail.com>
2017-12-20 21:41:03 -0500

    Sets font-family to Oswald

--- assets/js/react-app/index.js
+++ assets/js/react-app/index.js
@@ -7,10 +7,14 @@ import media from './styles/media';
 export default () => {
   //Set body color when application is rendered
   document.getElementsByTagName('body')[0].setAttribute('style', `background-color: ${colors.lightPink}`);
+
+  const AppContainer = styled.div`
+    font-family: 'Oswald', sans-serif;
+  `;
   
   return(
-    <div> 
+    <AppContainer>
       <Home />
-    </div>
+    </AppContainer>
   );
 }

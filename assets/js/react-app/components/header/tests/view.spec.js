558114dbb7f4f031082f00f7df760517c0ea165d
Travis Smith <travis.alexander.smith.ja@gmail.com>
2017-12-24 16:58:41 -0500

    Add unit test

--- assets/js/react-app/components/header/tests/view.spec.js
+++ assets/js/react-app/components/header/tests/view.spec.js
@@ -1,22 +1,30 @@
-import React from 'react';
-import { mount } from 'enzyme';
-import { expect } from 'chai';
+import React from "react";
+import { mount } from "enzyme";
+import { expect } from "chai";
 
-import Header, { Logo } from '..';
+import Header, { Logo } from "..";
 
-
-describe('Header', () => {
+describe("Header", () => {
   const wrapper = mount(<Header />);
-  it('renders', () => {
+  it("renders", () => {
     expect(wrapper).to.be.ok;
   });
-  it('contains a logo', () => {
-    expect(wrapper.find('img')).to.have.lengthOf(1);
+  it("contains a logo", () => {
+    expect(wrapper.find("img")).to.have.lengthOf(1);
   });
-  it('contains social media links', () => {
-    expect(wrapper.find('svg')).to.have.lengthOf(3);
+  it("contains social media links", () => {
+    expect(wrapper.find("svg")).to.have.lengthOf(3);
   });
-  it('contains the bakery location', () => {
-    expect(wrapper.find('#bakery-location')).to.exist;
+  it("contains the bakery location", () => {
+    expect(wrapper.find("#bakery-location")).to.exist;
+  });
+  it("contains a navbar", () => {
+    expect(wrapper.find("nav")).to.have.lengthOf(1);
+  });
+  describe("NavBar", () => {
+    it("contains 3 links", () => {
+      const navList = wrapper.find("nav ul");
+      expect(navList.children()).to.have.lengthOf(3);
+    });
   });
 });

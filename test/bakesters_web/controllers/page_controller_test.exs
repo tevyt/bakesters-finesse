fe518f8fef4bfc971b6c64a9e9570f4fdf384c81
Travis Smith <travis.alexander.smith.ja@gmail.com>
2018-02-05 22:35:51 -0500

    Updates elixir unit tests for new page id

--- test/bakesters_web/controllers/page_controller_test.exs
+++ test/bakesters_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule BakestersWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get conn, "/"
-    assert html_response(conn, 200) =~ "<div id=\"root\""
+    assert html_response(conn, 200) =~ "<div id=\"app\""
   end
 end

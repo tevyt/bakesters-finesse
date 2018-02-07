ce88f1be95321ee003efa8deaf0cc0b637dfd8bf
Travis Smith <travis.alexander.smith.ja@gmail.com>
2018-02-06 19:27:53 -0500

    Arranges grid of tiles

--- assets/js/react-app/screens/home.js
+++ assets/js/react-app/screens/home.js
@@ -1,7 +1,84 @@
 import React from "react"; //eslint-disable-line
 
 function Home() {
-  return <div>Home</div>;
+  return (
+    <section className="home section">
+      <div className="bakesters-logo">
+        <figure className="image is-3by2">
+          <img src="https://res.cloudinary.com/dv8aqe8lm/image/upload/c_scale,h_250,w_375/v1513737624/logo_hmncq7.png" />
+        </figure>
+      </div>
+      <div className="tile is-ancestor">
+        <div className="tile is-3 is-parent is-vertical">
+          <div className="tile is-child">
+            <figure className="image is-3by2">
+              <img src="http://res.cloudinary.com/dv8aqe8lm/image/upload/c_scale,w_320/v1515357309/WhatsApp_Image_2017-12-30_at_1.49.25_PM_xec6yl.jpg" />
+            </figure>
+          </div>
+          <div className="tile is-child">
+            <figure className="image is-3by2">
+              <img src="http://res.cloudinary.com/dv8aqe8lm/image/upload/c_scale,w_320/v1515357309/WhatsApp_Image_2017-12-30_at_1.49.25_PM_xec6yl.jpg" />
+            </figure>
+          </div>
+        </div>
+
+        <div className="tile is-3 is-parent is-vertical">
+          <div className="tile is-child">
+            <figure className="image is-3by2">
+              <img src="http://res.cloudinary.com/dv8aqe8lm/image/upload/c_scale,w_320/v1515357309/WhatsApp_Image_2017-12-30_at_1.49.25_PM_xec6yl.jpg" />
+            </figure>
+          </div>
+          <div className="tile is-child">
+            <figure className="image is-3by2">
+              <img src="http://res.cloudinary.com/dv8aqe8lm/image/upload/c_scale,w_320/v1515357309/WhatsApp_Image_2017-12-30_at_1.49.25_PM_xec6yl.jpg" />
+            </figure>
+          </div>
+        </div>
+
+        <div className="tile is-6 is-vertical">
+          <div className="tile">
+            <div className="tile is-parent">
+              <figure className="tile is-child image is-3by2">
+                <img src="http://res.cloudinary.com/dv8aqe8lm/image/upload/c_scale,w_320/v1515357309/WhatsApp_Image_2017-12-30_at_1.49.25_PM_xec6yl.jpg" />
+              </figure>
+            </div>
+            <div className="tile is-vertical">
+              <div className="tile is-parent">
+                <figure className="tile is-child image is-3by2">
+                  <img src="http://res.cloudinary.com/dv8aqe8lm/image/upload/c_scale,w_320/v1515357309/WhatsApp_Image_2017-12-30_at_1.49.25_PM_xec6yl.jpg" />
+                </figure>
+              </div>
+              <div className="tile">
+                <div className="tile is-parent">
+                  <figure className="tile is-child image is-3by2">
+                    <img src="http://res.cloudinary.com/dv8aqe8lm/image/upload/c_scale,w_320/v1515357309/WhatsApp_Image_2017-12-30_at_1.49.25_PM_xec6yl.jpg" />
+                  </figure>
+                </div>
+                <div className="tile is-parent">
+                  <figure className="tile is-child image is-3by2">
+                    <img src="http://res.cloudinary.com/dv8aqe8lm/image/upload/c_scale,w_320/v1515357309/WhatsApp_Image_2017-12-30_at_1.49.25_PM_xec6yl.jpg" />
+                  </figure>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="tile">
+            <figure className="tile is-child image is-3by2">
+              <img src="http://res.cloudinary.com/dv8aqe8lm/image/upload/c_scale,w_320/v1515357309/WhatsApp_Image_2017-12-30_at_1.49.25_PM_xec6yl.jpg" />
+            </figure>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
+
+// <div className="tile is-parent is-5 is-info">
+//   <div className="tile is-child">
+//     <figure className="image is-3by2">
+//       <img src="http://res.cloudinary.com/dv8aqe8lm/image/upload/c_scale,w_320/v1515357309/WhatsApp_Image_2017-12-30_at_1.49.25_PM_xec6yl.jpg" />
+//     </figure>
+//   </div>
+// </div>
 
 export default Home;

97ec3d690c1dd7bc08be6d1090e7105d22640742
Travis Smith <travis.alexander.smith.ja@gmail.com>
2018-02-09 10:11:50 -0500

    Adds pictures to photo grid

--- assets/js/react-app/screens/home.js
+++ assets/js/react-app/screens/home.js
@@ -3,60 +3,47 @@ import React from "react"; //eslint-disable-line
 function Home() {
   return (
     <section className="home section">
-      <div className="bakesters-logo">
-        <figure className="image is-3by2">
-          <img src="https://res.cloudinary.com/dv8aqe8lm/image/upload/c_scale,h_250,w_375/v1513737624/logo_hmncq7.png" />
-        </figure>
-      </div>
       <div className="tile is-ancestor">
         <div className="tile is-3 is-parent is-vertical">
-          <div className="tile is-child">
-            <figure className="image is-3by2">
-              <img src="http://res.cloudinary.com/dv8aqe8lm/image/upload/c_scale,w_320/v1515357309/WhatsApp_Image_2017-12-30_at_1.49.25_PM_xec6yl.jpg" />
-            </figure>
-          </div>
-          <div className="tile is-child">
-            <figure className="image is-3by2">
-              <img src="http://res.cloudinary.com/dv8aqe8lm/image/upload/c_scale,w_320/v1515357309/WhatsApp_Image_2017-12-30_at_1.49.25_PM_xec6yl.jpg" />
-            </figure>
-          </div>
+          <figure className="tile is-child image is-2by1">
+            <img src="https://res.cloudinary.com/dv8aqe8lm/image/upload/c_scale,w_1280/v1515357309/WhatsApp_Image_2017-12-30_at_1.49.25_PM_xec6yl.jpg" />
+          </figure>
+          <figure className="tile is-child image is-2by1">
+            <img src="https://res.cloudinary.com/dv8aqe8lm/image/upload/c_scale,w_1280/v1515357308/chocolate_cakies_phncpg.jpg" />
+          </figure>
         </div>
 
         <div className="tile is-3 is-parent is-vertical">
-          <div className="tile is-child">
-            <figure className="image is-3by2">
-              <img src="http://res.cloudinary.com/dv8aqe8lm/image/upload/c_scale,w_320/v1515357309/WhatsApp_Image_2017-12-30_at_1.49.25_PM_xec6yl.jpg" />
-            </figure>
-          </div>
-          <div className="tile is-child">
-            <figure className="image is-3by2">
-              <img src="http://res.cloudinary.com/dv8aqe8lm/image/upload/c_scale,w_320/v1515357309/WhatsApp_Image_2017-12-30_at_1.49.25_PM_xec6yl.jpg" />
-            </figure>
-          </div>
+          <figure className="tile is-child image is-2by1">
+            <img src="https://res.cloudinary.com/dv8aqe8lm/image/upload/c_scale,w_1280/v1515357301/WhatsApp_Image_2017-12-30_at_1.49.33_PM_jufrad.jpg" />
+          </figure>
+          <figure className="tile is-child image is-2by1">
+            <img src="https://res.cloudinary.com/dv8aqe8lm/image/upload/c_scale,w_1280/v1515357305/kymone_birthday_cake_grwdqi.jpg" />
+          </figure>
         </div>
 
         <div className="tile is-6 is-vertical">
           <div className="tile">
             <div className="tile is-parent">
-              <figure className="tile is-child image is-3by2">
-                <img src="http://res.cloudinary.com/dv8aqe8lm/image/upload/c_scale,w_320/v1515357309/WhatsApp_Image_2017-12-30_at_1.49.25_PM_xec6yl.jpg" />
+              <figure className="tile is-child image is-1by1">
+                <img src="https://res.cloudinary.com/dv8aqe8lm/image/upload/c_scale,w_1280/v1518188376/WhatsApp_Image_2017-12-30_at_1.49.25_PM_1_nmyrbe.jpg" />
               </figure>
             </div>
             <div className="tile is-vertical">
               <div className="tile is-parent">
-                <figure className="tile is-child image is-3by2">
-                  <img src="http://res.cloudinary.com/dv8aqe8lm/image/upload/c_scale,w_320/v1515357309/WhatsApp_Image_2017-12-30_at_1.49.25_PM_xec6yl.jpg" />
+                <figure className="tile is-child image is-2by1">
+                  <img src="https://res.cloudinary.com/dv8aqe8lm/image/upload/c_scale,w_1280/v1515357304/WhatsApp_Image_2017-12-30_at_1.49.27_PM_1_xjwzkf.jpg" />
                 </figure>
               </div>
               <div className="tile">
                 <div className="tile is-parent">
-                  <figure className="tile is-child image is-3by2">
-                    <img src="http://res.cloudinary.com/dv8aqe8lm/image/upload/c_scale,w_320/v1515357309/WhatsApp_Image_2017-12-30_at_1.49.25_PM_xec6yl.jpg" />
+                  <figure className="tile is-child image is-2by1">
+                    <img src="https://res.cloudinary.com/dv8aqe8lm/image/upload/c_scale,w_1280/v1515357303/WhatsApp_Image_2017-12-30_at_1.49.27_PM_ypba0m.jpg" />
                   </figure>
                 </div>
                 <div className="tile is-parent">
-                  <figure className="tile is-child image is-3by2">
-                    <img src="http://res.cloudinary.com/dv8aqe8lm/image/upload/c_scale,w_320/v1515357309/WhatsApp_Image_2017-12-30_at_1.49.25_PM_xec6yl.jpg" />
+                  <figure className="tile is-child image is-2by1">
+                    <img src="https://res.cloudinary.com/dv8aqe8lm/image/upload/c_scale,w_1280/v1515357303/WhatsApp_Image_2017-12-30_at_1.49.29_PM_n7njby.jpg" />
                   </figure>
                 </div>
               </div>
@@ -64,7 +51,7 @@ function Home() {
           </div>
           <div className="tile">
             <figure className="tile is-child image is-3by2">
-              <img src="http://res.cloudinary.com/dv8aqe8lm/image/upload/c_scale,w_320/v1515357309/WhatsApp_Image_2017-12-30_at_1.49.25_PM_xec6yl.jpg" />
+              <img src="https://res.cloudinary.com/dv8aqe8lm/image/upload/c_scale,w_1280/v1513737624/logo_hmncq7.png" />
             </figure>
           </div>
         </div>
@@ -72,13 +59,5 @@ function Home() {
     </section>
   );
 }
-
-// <div className="tile is-parent is-5 is-info">
-//   <div className="tile is-child">
-//     <figure className="image is-3by2">
-//       <img src="http://res.cloudinary.com/dv8aqe8lm/image/upload/c_scale,w_320/v1515357309/WhatsApp_Image_2017-12-30_at_1.49.25_PM_xec6yl.jpg" />
-//     </figure>
-//   </div>
-// </div>
 
 export default Home;

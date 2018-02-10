bd07f37b65aa9a4147a64e92ec4687173c636d4b
Travis Smith <travis.alexander.smith.ja@gmail.com>
2018-02-09 23:27:54 -0500

    Creates contact form

--- assets/js/react-app/screens/contact.js
+++ assets/js/react-app/screens/contact.js
@@ -14,23 +14,9 @@ function Contact() {
         If you would like to place an order or enquire about our products and
         services you may contact using any of the methods below.
       </p>
-      <div className="contact-options">
-        <ContactOption
-          href="tel:18764739816"
-          text="18764739816"
-          Icon={PhoneInTalkIcon}
-        />
-        <ContactOption
-          href="https://www.instagram.com/bakestersfinesse/"
-          Icon={InstagramIcon}
-          text="Send us a message on Instagram."
-        />
-        <ContactOption
-          href="mailto:bakestersfinesse@gmail.com"
-          text="Send us an email: bakestersfinesse@gmail.com"
-          Icon={EmailIcon}
-        />
-      </div>
+      <ContactOptions />
+      Or leave a message below...
+      <ContactForm />
     </section>
   );
 }
@@ -43,6 +29,82 @@ function ContactOption({ href, text, Icon }) {
       </CircleIcon>
       <a href={href}>{text}</a>
     </div>
+  );
+}
+
+function ContactOptions() {
+  return (
+    <div className="contact-options">
+      <ContactOption
+        href="tel:18764739816"
+        text="18764739816"
+        Icon={PhoneInTalkIcon}
+      />
+      <ContactOption
+        href="https://www.instagram.com/bakestersfinesse/"
+        Icon={InstagramIcon}
+        text="Send us a message on Instagram."
+      />
+      <ContactOption
+        href="mailto:bakestersfinesse@gmail.com"
+        text="Send us an email: bakestersfinesse@gmail.com"
+        Icon={EmailIcon}
+      />
+    </div>
+  );
+}
+
+function ContactForm() {
+  return (
+    <form
+      className="contact-form"
+      onSubmit={e => {
+        e.preventDefault();
+        console.log("Form submitted");
+      }}
+    >
+      <div className="field">
+        <label className="label">Name</label>
+        <div className="control">
+          <input className="input" type="text" placeholder="Your Name" />
+        </div>
+      </div>
+      <div className="field">
+        <label className="label">Email</label>
+        <div className="control has-icons-left">
+          <input
+            className="input"
+            type="email"
+            placeholder="Your Email Address"
+          />
+          <span className="icon is-small is-left">
+            <EmailIcon />
+          </span>
+        </div>
+      </div>
+      <div className="field">
+        <label className="label">Phone</label>
+        <div className="control has-icons-left">
+          <input className="input" type="tel" placeholder="Your Phone Number" />
+          <span className="icon is-small is-left">
+            <PhoneInTalkIcon />
+          </span>
+        </div>
+      </div>
+      <div className="field">
+        <label className="label">Message</label>
+        <div className="control">
+          <textarea className="textarea" placeholder="Leave a message" />
+        </div>
+      </div>
+      <div className="field">
+        <div className="control">
+          <button type="submit" className="button is-link">
+            Send
+          </button>
+        </div>
+      </div>
+    </form>
   );
 }
 

6382dcfc000aa7a9cd0a5d181b2b1d993bb2ccc8
Travis Smith <travis.alexander.smith.ja@gmail.com>
2018-02-28 19:36:35 -0500

    Changes errors to warnings

--- assets/.eslintrc.js
+++ assets/.eslintrc.js
@@ -13,9 +13,9 @@ module.exports = {
   },
   plugins: ["react"],
   rules: {
-    indent: ["error", 2],
+    indent: ["warn", 2],
     "linebreak-style": ["error", "unix"],
-    quotes: ["error", "double"],
-    semi: ["error", "always"]
+    quotes: ["warn", "double"],
+    semi: ["warn", "always"]
   }
 };

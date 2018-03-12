b17033623c84c27b054365bd88ce2819e63bfbef
Travis Smith <travis.alexander.smith.ja@gmail.com>
2018-03-11 19:14:39 -0500

    Updates config

--- config/config.exs
+++ config/config.exs
@@ -6,29 +6,25 @@
 use Mix.Config
 
 # General application configuration
-config :bakesters,
-  ecto_repos: [Bakesters.Repo]
+config :bakesters, ecto_repos: [Bakesters.Repo]
 
 # Configures the endpoint
 config :bakesters, BakestersWeb.Endpoint,
   url: [host: "localhost"],
   secret_key_base: "V7YkLR9KrPff5TdEJK0ofPJ/Grvr1yR1zG3gkNBLaQWYYqyxEzfk1KXzFbFQ351N",
   render_errors: [view: BakestersWeb.ErrorView, accepts: ~w(html json)],
-  pubsub: [name: Bakesters.PubSub,
-           adapter: Phoenix.PubSub.PG2]
+  pubsub: [name: Bakesters.PubSub, adapter: Phoenix.PubSub.PG2]
 
 # Configures Elixir's Logger
 config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
-IO.puts "MAILGUN_API_KEY: #{System.get_env("MAILGUN_API_KEY")}"
 config :bakesters, Bakesters.Mailer,
   adapter: Bamboo.MailgunAdapter,
   domain: System.get_env("MAILGUN_DOMAIN"),
   api_key: System.get_env("MAILGUN_API_KEY")
 
-
 # Import environment specific config. This must remain at the bottom
 # of this file so it overrides the configuration defined above.
-import_config "#{Mix.env}.exs"
+import_config "#{Mix.env()}.exs"
